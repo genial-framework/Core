35868a62d748b0e9bba35c518fb8fd2b6cbabd18
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-16 09:44:30 -0500

    Add files via upload

---
+++ src/Exception/LengthException.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Core> For the canonical source repository.
+ *
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Core/blob/master/LICENSE> New BSD License.
+ */
+
+namespace Genial\Core;
+
+/**
+ * Utils.
+ */
+class Utils
+{
+    /**
+     * getCoreParams().
+     *
+     * Get the currently set cookie params.
+     * 
+     * @return array Return the currently set cookie params.
+     */
+    public static function getCoreParams()
+    {
+        return session_get_cookie_params();
+    }
+}
