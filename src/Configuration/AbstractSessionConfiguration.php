f6572648788b0a543de60851dc6dd2113ae6233a
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-19 14:00:14 -0500

    Update AbstractSessionConfiguration.php

--- src/Configuration/AbstractSessionConfiguration.php
+++ src/Configuration/AbstractSessionConfiguration.php
@@ -1,1 +1,58 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Core> For the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Core/blob/master/LICENSE> New BSD License.
+ */
+ 
+namespace Genial\Configuration;
 
+use Genial\Core\Exception;
+
+/**
+ * AbstractSessionConfiguration.
+ */
+abstract class AbstractSessionConfiguration implements ConfigurationInterface, SessionConfigurationInterface
+{
+    /**
+     * @var array|[] $configuration The session configuration array. 
+     */
+    protected $configuration = [];
+    /**
+     * Get and set the configuration.
+     *
+     * @throws BadFunctionCallException If the `env()` function does not exist.
+     *
+     * @return bool|true Returns true if the configuration was set correctly.
+     */
+    function __construct()
+    {
+        if (!function_exists('env'))
+        {
+            throw new Exception\BadFunctionCallException(sprintf(
+                '`%s` The `env()` function does not exist.',
+                __METHOD__
+            ));
+        }
+        $this->configuration = env('session');
+    }
+    /**
+     * Run the configuration.
+     *
+     * @return bool|true Returns true if the configuration ran successfully.
+     */
+    public function run()
+    {
+        $this->setSessionName();
+        $this->setSessionSaveHandler();
+        $this->setSessionCookieParams();
+        $this->setSessionGarbageCollector();
+        $this->setSessionSecurity();
+        return true;
+    }
+}
