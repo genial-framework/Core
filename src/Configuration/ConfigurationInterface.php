9606f4092f3e7303bb78409bd386254817971c63
Nicholas English <30304307+Nenglish7@users.noreply.github.com>
2017-12-19 13:52:59 -0500

    Create ConfigurationInterface.php

---
+++ src/Configuration/ConfigurationInterface.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Genial Framework.
+ *
+ * @author    Nicholas English <https://github.com/Nenglish7>
+ * @author    Genial Contributors <https://github.com/orgs/Genial-Framework/people>
+ *
+ * @link      <https://github.com/Genial-Framework/Core> For the canonical source repository.
+ * @copyright Copyright (c) 2017-2018 Genial Framework. <https://github.com/Genial-Framework>
+ * @license   <https://github.com/Genial-Framework/Core/blob/master/LICENSE> New BSD License.
+ */
+
+namespace Genial\Configuration;
+
+/**
+ * ConfigurationInterface.
+ */
+interface ConfigurationInterface
+{
+    /**
+     * Get and set the configuration.
+     *
+     * @throws BadFunctionCallException If the `env()` function does not exist.
+     *
+     * @return bool|true Returns true if the configuration was set correctly.
+     */
+    function __construct();
+    /**
+     * Run the configuration.
+     *
+     * @return bool|true Returns true if the configuration ran successfully.
+     */
+    public function run();
+}
